746d35e8d10fe1b3ed0f2892971b63063f906623
alinaelvn <alina.elvn@gmail.com>
2025-07-12 21:39:30 -0400

    added sys argument for bookpath input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_characters
 
 def get_book_text(filepath):
@@ -6,7 +7,13 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
 
     num_words = count_words(book_text)
     print (f"{num_words} words found in the document")
@@ -17,7 +24,7 @@ def main():
     # report
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
